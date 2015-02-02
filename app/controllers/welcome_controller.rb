1dfe6dd440f07ddbc372e95f95d3629654afb71d
Kyle C Fix <kylecfix@kyles-mbp.cb.careerbuilder.com\032careerbuilder.com>
2015-02-02 18:03:27 -0500

    updated background and added nav bar

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,22 @@
 class WelcomeController < ApplicationController
+  before_action :set_name
+
   def index
   end
 
   def about
+  end
+
+  def contact
+  end
+  
+  private
+  def set_name
+  	@name = params[:name]
+  	if @name
+  		session[:name] = @name
+  	else
+  		@name = session[:name]
+  	end	
   end
 end
