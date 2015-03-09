53b62ac2d2a615e8dda718724e3d535a4b56059f
Kyle C Fix <kylecfix@Kyles-MacBook-Pro.local>
2015-03-09 10:36:00 -0400

    Styling Portfolio

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204193932) do
+ActiveRecord::Schema.define(version: 20150308010754) do
 
   create_table "works", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "url"
   end
 
 end
